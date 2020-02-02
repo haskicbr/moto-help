9f5afde03d33f629ce618829ccb2ff936526a93c
Haskicbr <haskicbr@gmail.com>
2020-02-02 23:39:03 +0300

    webpack file-loader

--- webpack-server.js
+++ webpack-server.js
@@ -39,6 +39,42 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.s(c|a)ss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        // Requires sass-loader@^8.0.0
+                        options: {
+                            implementation: require('sass'),
+                        },
+                    }
+                ],
+            },
+
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+/*                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }*/
+                    }
+                ]
             }
         ],
     },
