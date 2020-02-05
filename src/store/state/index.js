d5b3f71aa718750e5dbfd34e4c161e92e3ea2631
Haskicbr <haskicbr@gmail.com>
2020-02-05 17:47:00 +0300

    upd clear state

--- src/store/state/index.js
+++ src/store/state/index.js
@@ -1,18 +1,6 @@
 const state = {
-
-    count: 0,
-
     mileages: [],
     currentMileage: 0,
-
-    todos: [
-        {id: 1, text: '...', done: true},
-        {id: 2, text: '...', done: false}
-    ],
-    testComputed: {
-        computedValue: 'this is computed value'
-    },
-
     isLogged: false
 };
 
