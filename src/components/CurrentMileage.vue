2a23b08569f9ba091212224ee25ea3be405792ba
haskicbr <haskicbr@gmail.com>
2020-02-22 01:31:04 +0300

    change current mileage page

--- src/components/CurrentMileage.vue
+++ src/components/CurrentMileage.vue
@@ -93,67 +93,88 @@
 
 
         <div class="md-headline" v-if="!isEditable" style="vertical-align: middle; display: flex; align-self: center">
-            <div style="display: flex; align-items: center">current mileage: {{$store.state.currentMileage}} km</div>
+            <div class="title" style="display: flex; align-items: center">current mileage: {{$store.state.currentMileage}} km</div>
 
-            <v-btn v-on:click="isEditable = !isEditable" class="mx-2" fab dark small color="cyan">
-                <v-icon dark>mdi-pencil</v-icon>
-            </v-btn>
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                    <v-btn  v-on="on" v-on:click="isEditable = !isEditable" class="mx-2" fab dark small color="primary">
+                        <v-icon dark>mdi-pencil</v-icon>
+                    </v-btn>
+                </template>
+                <span>change mileage</span>
+            </v-tooltip>
 
-            <router-link tag="div" to="/service/add">
-                <v-btn class="mx-1" fab dark small color="cyan">
-                    <v-icon dark>mdi-plus</v-icon>
-                </v-btn>
-            </router-link>
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                    <router-link tag="div" to="/service/add">
+                        <v-btn v-on="on"  class="mx-1" fab dark small color="primary">
+                            <v-icon dark>mdi-plus</v-icon>
+                        </v-btn>
+                    </router-link>
+                </template>
+
+                <span>add service</span>
+            </v-tooltip>
         </div>
+
 
 
         <div v-if="isEditable" class="md-layout md-gutter">
             <div class="md-layout-item md-xsmall-size-100  md-medium-size-25 md-size-25">
 
                 <div>
-                    <label>Change current mileage</label>
+                    <label class="title">Change current mileage</label>
 
-                    <v-text-field type="number" v-model="currentMileage"></v-text-field>
+                    <v-text-field style="font-size: 24px" type="number" v-model="currentMileage"></v-text-field>
                 </div>
 
-                <button v-on:click="changeCurrent" class="md-layout-item md-size-100 md-raised md-primary">
-                    Save
-                </button>
+                <v-btn v-on:click="changeCurrent" color="primary">Save</v-btn>
+
             </div>
         </div>
 
+        <br>
 
         <div v-if="getServices() !== 0" class="md-layout md-gutter">
-            <div class="">
-                <div class="md-double-line md-elevation-4">
+            <v-card max-width="700px">
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title class="title">Maintenance</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
 
-                    <h1>Maintenance</h1>
+                <v-list-item two-line>
+                    <v-list-item-content>
+                        <v-list-item-title></v-list-item-title>
+                        <v-list-item-subtitle></v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
 
-                    <template v-for="(mileage) in getServices()">
+                <template v-for="(mileage) in getServices()">
+                    <v-list-item three-line>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                Lifetime {{mileage.lifetime}} km
+                            </v-list-item-title>
 
+                            <v-list-item-title>
+                                Change date {{dateFormat(mileage.date)}}
+                            </v-list-item-title>
 
-                        <div class="md-list-item-text">
-
-                            <br/>
-
-                            <span>lifetime {{mileage.lifetime}} km</span>
-                            <span>change date {{dateFormat(mileage.date)}}</span>
-                            <span>change mileage {{mileage.mileage}} km</span>
-
-                            <br/>
+                            <v-list-item-title>
+                                Change mileage {{mileage.mileage}} km
+                            </v-list-item-title>
 
                             <v-progress-linear
                                     :color="getProgressColor(getMileageProgress(mileage.mileage, mileage.lifetime))"
                                     :value="getMileageProgress(mileage.mileage, mileage.lifetime)"
                                     height="10"
                             ></v-progress-linear>
-                        </div>
 
-                    </template>
-
-                </div>
-            </div>
-
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-card>
         </div>
     </div>
 
