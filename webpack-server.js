71e260786f1d24e5e26fdcdfded22cedcd514f32
haskicbr <haskicbr@gmail.com>
2020-02-27 06:12:46 +0300

    add allow-origin webpack

--- webpack-server.js
+++ webpack-server.js
@@ -85,6 +85,12 @@ module.exports = {
     devtool: false,
 
     devServer: {
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization, x-id, Content-Length, X-Requested-With",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+        },
         contentBase: path.join(__dirname, './public'),
         host: '0.0.0.0',
         disableHostCheck: true,
