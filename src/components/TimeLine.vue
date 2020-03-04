e44c2130e77045f6a5dc4b01177c201cf967f0a8
haskicbr <haskicbr@gmail.com>
2020-03-04 20:00:59 +0300

    change timeline with colors, changes etc

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -10,7 +10,52 @@
                 return this.$store.state.services.filter((item) => {
                     return item.date === date
                 })
-            }
+            },
+            getMileageDescription(type) {
+                return this.$store.state.serviceTypes[type].description;
+            },
+
+            getMileageProgress(mileage, lifetime, currentMileage) {
+
+                let compareMileage = currentMileage - mileage;
+
+                let progress = compareMileage / lifetime * 100;
+
+                if (progress > 100) {
+                    return 100;
+                }
+
+                if (progress < 0) {
+                    return 0;
+                }
+
+                return parseInt(progress)
+            },
+
+            getProgressColor(progress) {
+
+                if (progress >= 90) {
+                    return "red";
+                } else if (progress >= 70) {
+                    return "orange";
+                }
+
+                return "blue";
+            },
+            getChangeThought(mileage, currentMileage) {
+
+                if (mileage.mileage > currentMileage) {
+                    return mileage.lifetime;
+                }
+
+                let change = mileage.lifetime + mileage.mileage - currentMileage;
+
+                if (change < 0) {
+                    return 0;
+                }
+
+                return change;
+            },
 
         },
 
@@ -50,9 +95,50 @@
                 <span slot="opposite"><span class="title">{{key}} <br>{{val}} km</span></span>
 
                 <v-card v-if="getServicesByDate(key).length > 0" class="">
-                    <v-card-title class="headline">Lorem ipsum</v-card-title>
-                    <div >
-                        {{getServicesByDate(key)}}
+                    <div v-for="service in getServicesByDate(key)">
+
+                        <v-list-item three-line>
+                            <v-list-item-content class="justify-center">
+
+                                <v-list-item-content class="justify-center">
+                                    <div class="v-btn">
+                                        {{getMileageDescription(service.type)}}
+
+                                    </div>
+                                </v-list-item-content>
+
+
+                                {{(() => {
+                                    progeressColor = getProgressColor(getMileageProgress(service.mileage, service.lifetime, val))
+                                })()}}
+
+                                <v-list-item class="justify-center flex-wrap">
+                                    <v-btn text>
+                                        <span>{{getChangeThought(service, val)}} km</span>
+                                        <v-icon class="mx-1" :color="progeressColor">mdi-cog-clockwise</v-icon>
+                                    </v-btn>
+
+                                    <v-btn text>
+                                        <span>{{service.lifetime}} km</span>
+                                        <v-icon class="mx-1" :color="progeressColor">mdi-cogs</v-icon>
+                                    </v-btn>
+
+                                    <v-btn text>
+                                        <span>{{service.mileage}} km</span>
+                                        <v-icon class="mx-1" icon="mdi-lock" :color="progeressColor">mdi-wrench</v-icon>
+                                    </v-btn>
+                                </v-list-item>
+                                <!--
+                                                                <v-progress-linear
+                                                                        :color="progeressColor"
+                                                                        :value="getMileageProgress(mileage.mileage, mileage.lifetime)"
+                                                                        height="10"
+                                                                ></v-progress-linear>-->
+
+                            </v-list-item-content>
+                        </v-list-item>
+
+
                     </div>
                 </v-card>
             </v-timeline-item>
