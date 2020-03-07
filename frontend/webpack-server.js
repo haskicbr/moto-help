bf84bcd99d65686647b31a207792cf47d1ebcadf
haskicbr <haskicbr@gmail.com>
2020-03-08 00:01:31 +0300

    upd devtool

--- frontend/webpack-server.js
+++ frontend/webpack-server.js
@@ -82,7 +82,7 @@ module.exports = {
         new VueLoaderPlugin(),
         new webpack.SourceMapDevToolPlugin({})
     ],
-    devtool: false,
+    devtool: 'inline-source-map',
 
     devServer: {
         headers: {
