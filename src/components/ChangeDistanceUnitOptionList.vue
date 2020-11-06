19174531dd55514c0a6aa759c9825138ff6f91c2
Vladislav <haskicbr@gmail.com>
2020-11-07 00:23:20 +0300

    add distanceUnit optionList component

--- src/components/ChangeDistanceUnitOptionList.vue
+++ src/components/ChangeDistanceUnitOptionList.vue
@@ -1,13 +1,47 @@
-<template>
-$END$
-</template>
-
 <script>
+import {DistanceTypes, SettingsTypes} from "../store/state/SettingsTypes";
+import {SettingsActionTypes} from "../store/actions/types";
+
 export default {
-name: "ChangeDistanceUnitOptionList"
+    name: "ChangeDistanceUnitOptionList",
+
+    methods: {
+        changeDistanceUnit(distanceUnit) {
+            this.$store.dispatch(SettingsActionTypes.CHANGE_DISTANCE_UNIT, {
+                distanceUnit
+            })
+        }
+    },
+
+    computed: {
+        distanceOptionList() {
+            const distances = SettingsTypes.UNITS.DISTANCE;
+            const list = [];
+
+
+            for (let key in distances) {
+                list.push({
+                    key, description: distances[key]
+                });
+            }
+            return list;
+        }
+    },
+    data() {
+        return {
+            distanceUnit: this.$store.state.settings.units.distance,
+        }
+    }
 }
 </script>
 
-<style scoped>
-
-</style>
+<template>
+    <v-select
+        v-on:change="changeDistanceUnit(distanceUnit)"
+        :items="distanceOptionList"
+        item-text="description"
+        item-value="key"
+        v-model="distanceUnit"
+        :label="$store.getters.languages('DISTANCE_UNIT')"
+    ></v-select>
+</template>
