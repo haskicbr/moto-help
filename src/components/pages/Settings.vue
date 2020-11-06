089c46a710e1ea88cf283cdab0f7e297437294b3
Vladislav <haskicbr@gmail.com>
2020-11-07 00:23:35 +0300

    add distanceUnit optionList component

--- src/components/pages/Settings.vue
+++ src/components/pages/Settings.vue
@@ -1,45 +1,21 @@
 <script>
-
-import {DistanceTypes, SettingsTypes} from "../../store/state/SettingsTypes";
 import ChangeLanguageOptionList from "../ChangeLanguageOptionList";
+import ChangeDistanceUnitOptionList from "../ChangeDistanceUnitOptionList";
 
 export default {
     name: "Settings",
-    components: {ChangeLanguageOptionList},
-    computed: {
-        distanceOptionList() {
-            const distances = SettingsTypes.UNITS.DISTANCE;
-            const list = [];
-            for (let key in distances) {
-                list.push({
-                    key, description: distances[key]
-                });
-            }
-            return list;
-        }
+    components: {
+        ChangeDistanceUnitOptionList,
+        ChangeLanguageOptionList
     },
-    data() {
-        return {
-            distanceUnit: DistanceTypes.KM
-        }
-    }
 }
 </script>
 
 <template>
     <v-list-item>
         <v-list-item-content>
-
             <ChangeLanguageOptionList/>
-            <v-select
-                v-on:change=""
-                :items="distanceOptionList"
-                item-text="description"
-                item-value="key"
-                v-model="distanceUnit"
-                label="Distance unit"
-            ></v-select>
-            {{ distanceOptionList }}
+            <ChangeDistanceUnitOptionList/>
         </v-list-item-content>
     </v-list-item>
 
