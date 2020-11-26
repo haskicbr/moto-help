f3abe08a761951e0409904f5537eb44f7db25943
Vladislav <haskicbr@gmail.com>
2020-11-26 04:44:07 +0300

    fix clearTransportForm

--- src/components/pages/Transport.vue
+++ src/components/pages/Transport.vue
@@ -63,6 +63,7 @@ export default {
         },
 
         clearTransportForm() {
+            this.transportId = "";
             this.$refs.form.reset();
             this.$refs.form.resetValidation();
         },
