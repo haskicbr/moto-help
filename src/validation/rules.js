e8bdea0cb74f5dcba28f1c3610e6c46e180b9aa1
Vladislav <haskicbr@gmail.com>
2020-11-26 04:22:28 +0300

    new validation rules

--- src/validation/rules.js
+++ src/validation/rules.js
@@ -4,18 +4,52 @@ export default {
     email: () => {
         return [
             v => !!v || 'E-mail is required',
-            v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            v => /.+@.+\..+/.test(v) || 'The field must be valid E-mail address',
         ]
     },
     min: (length) => {
-        return [v => v.length >= length || `Min ${length} characters`]
+        return [v => {
+
+            if (typeof (v) === 'string') {
+                return v.length >= length || `Min ${length} characters`
+            } else {
+                return v >= length || `Min ${length}`
+            }
+        }]
     },
     max: (length) => {
-        return [v => (v && v.length <= length) || `Name must be less than ${length} characters`]
+        return [
+            v => {
+                if (typeof (v) === 'string') {
+                    return (v && v.length <= length) || `Max ${length} characters`
+                } else {
+                    console.log(v)
+                    return (v && v <= length) || `Max ${length}`
+                }
+            }
+        ]
     },
+    maxNumber: (max) => {
+        return [
+            v => {
+                v = Number(v);
+                return (!isNaN(v) && v <= max) || `Max ${max}`;
+            }
+        ]
+    },
+
+    minNumber: (min) => {
+        return [
+            v => {
+                v = Number(v);
+                return (!isNaN(v) && v >= min) || `Max ${min}`;
+            }
+        ]
+    },
+
     required: () => {
         return [
-            v => !!v || 'Name is required',
+            v => !!v || 'Field is required',
         ]
     }
 }
